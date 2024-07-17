a21676e8f9184e75603dc9e184a97e0fb5c9cf62
superzero4 <78915984+superzero4@users.noreply.github.com>
2024-07-17 21:37:07 +0200

    Uncompiling approeahc of filters with templates

--- Project/include/ImageTreatments.hpp
+++ Project/include/ImageTreatments.hpp
@@ -1,25 +1,31 @@
 #ifndef PROJECTCPP_IMAGETREATMENTS_HPP
 #define PROJECTCPP_IMAGETREATMENTS_HPP
+
 #include "Image.hpp"
 #include "ImageIterator.hpp"
-const float sobelFilterMatrix[3][3] = {
-        {-1, 0, 1},
-        {-2, 0, 2},
-        {-1, 0, 1}
-};
+
 template<int N>
-Image genericFilter(const Image &img,const float (&filter)[N][N]);
-inline Image sobelFilter(const Image &img){
-    return genericFilter(img, sobelFilterMatrix);
+Image genericFilter(const Image &img, std::array<std::array<float, N>, N> filter);
+
+inline Image sobelFilter(const Image &img) {
+//std::array<std::array<float,static_cast<size_t>(3)>,static_cast<size_t>(3)> sobelFilterMatrix;
+    //={
+    //        {-1, 0, 1},
+    //        {-2, 0, 2},
+    //        {-1, 0, 1}
+    //};
+    return Image();// genericFilter(img, sobelFilterMatrix);
 }
+
 template<int N>
-inline Image medianBlur(const Image &img){
-    const float filter[N][N];
+inline Image medianBlur(const Image &img) {
+    std::array<std::array<float, N>, N> filter;
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
-            filter[i][j] = 1.f/(N*N);
+            filter[i][j] = 1.f / (N * N);
         }
     }
     return genericFilter(img, filter);
 }
+
 #endif //PROJECTCPP_IMAGETREATMENTS_HPP
--- Project/src/ImageTreatments.cpp
+++ Project/src/ImageTreatments.cpp
@@ -1,18 +1,18 @@
 #include "ImageTreatments.hpp"
 
 template<int N>
-Image genericFilter(const Image &img, const float (&filter)[N][N]) {
+Image genericFilter(const Image &img,std::array<std::array<float,N>,N> filter){
     ImageIterator it(img);
     ImageIterator en = end(img);
     Image copy(img);
-    for (; it != en; it++) {
+    for (; it != en; ++it) {
         int x = it.getRelativeCol();
         int y = it.getRelativeRow();
         int c = it.getRelativeChannel();
         float sum = 0;
         //N is odd, N/2 will floor
         auto [filterIt, endIterator] = it.filterIterators(N / 2);
-        for (; filterIt != endIterator; filterIt++)
+        for (; filterIt != endIterator; ++filterIt)
             sum += *filterIt * filter[filterIt.getRelativeCol()][filterIt.getRelativeRow()];
         copy(x, y, c) = static_cast<int>(sum);
     }
