fdaa8cca4e7748dff73014d06f014de64e176f1c
superzero4 <78915984+superzero4@users.noreply.github.com>
2024-07-17 22:36:09 +0200

    Compiling approach "without" templates and with vectors

--- Project/include/ImageTreatments.hpp
+++ Project/include/ImageTreatments.hpp
@@ -4,28 +4,22 @@
 #include "Image.hpp"
 #include "ImageIterator.hpp"
 
-template<int N>
-Image genericFilter(const Image &img, std::array<std::array<float, N>, N> filter);
+Image genericFilter(const Image &img, std::vector<std::vector<float>> filter,bool normalizeFilterByEffactive = false);
 
 inline Image sobelFilter(const Image &img) {
-//std::array<std::array<float,static_cast<size_t>(3)>,static_cast<size_t>(3)> sobelFilterMatrix;
-    //={
-    //        {-1, 0, 1},
-    //        {-2, 0, 2},
-    //        {-1, 0, 1}
-    //};
-    return Image();// genericFilter(img, sobelFilterMatrix);
+    std::vector<std::vector<float>> sobelFilterMatrix =
+            {
+                    {-1, 0, 1},
+                    {-2, 0, 2},
+                    {-1, 0, 1}
+            };
+    return genericFilter(img, sobelFilterMatrix);
 }
 
 template<int N>
 inline Image medianBlur(const Image &img) {
-    std::array<std::array<float, N>, N> filter;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            filter[i][j] = 1.f / (N * N);
-        }
-    }
-    return genericFilter(img, filter);
+    std::vector<std::vector<float>> filter(N, std::vector<float>(N,1.f));
+    return genericFilter(img, filter,true);
 }
 
 #endif //PROJECTCPP_IMAGETREATMENTS_HPP
--- Project/src/ImageTreatments.cpp
+++ Project/src/ImageTreatments.cpp
@@ -1,7 +1,10 @@
 #include "ImageTreatments.hpp"
 
-template<int N>
-Image genericFilter(const Image &img,std::array<std::array<float,N>,N> filter){
+Image genericFilter(const Image &img, std::vector<std::vector<float>> filter,bool normalizeFilterByEffactive) {
+    int N = filter.size();
+    if (N%2==0 || !std::all_of(filter.begin(), filter.end(),
+                     [N](auto &v) { return v.size() == N; }))
+        throw "Filter must be square and odd sized";
     ImageIterator it(img);
     ImageIterator en = end(img);
     Image copy(img);
@@ -12,9 +15,10 @@ Image genericFilter(const Image &img,std::array<std::array<float,N>,N> filter){
         float sum = 0;
         //N is odd, N/2 will floor
         auto [filterIt, endIterator] = it.filterIterators(N / 2);
-        for (; filterIt != endIterator; ++filterIt)
-            sum += *filterIt * filter[filterIt.getRelativeCol()][filterIt.getRelativeRow()];
-        copy(x, y, c) = static_cast<int>(sum);
+        int norm=0;
+        for (; filterIt != endIterator; ++filterIt, ++norm)
+            sum += (*filterIt) * filter[filterIt.getRelativeCol()][filterIt.getRelativeRow()];
+        copy(x, y, c) = static_cast<int>(sum)/(normalizeFilterByEffactive && norm > 0 ? norm : 1.f);
     }
     return copy;
 }
