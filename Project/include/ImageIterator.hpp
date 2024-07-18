a40651038ae0f35358addae261412c097a0cfcb8
superzero4 <78915984+superzero4@users.noreply.github.com>
2024-07-19 01:38:41 +0200

    Switch iterator to work with Image const pointer instead of const ref

--- Project/include/ImageIterator.hpp
+++ Project/include/ImageIterator.hpp
@@ -10,11 +10,12 @@
 typedef uint8_t pixel;
 
 class ImageIterator {
-	typedef std::forward_iterator_tag iterator_category;
-	typedef pixel value_type;
-	typedef pixel difference_type;
-	typedef pixel *pointer;
-	typedef pixel &reference;
+public:
+	using iterator_category = std::forward_iterator_tag;
+	using value_type = pixel;
+	using difference_type = std::ptrdiff_t;
+	using pointer = const pixel *;
+	using reference = const pixel &;
 
 private:
 	int startRow;
@@ -25,7 +26,7 @@ private:
 	int startChannel;
 	int channel;
 	int amountOfChannel;
-	const Image &indexed;
+	Image const *indexed;
 
 public:
 	int getRelativeRow() const {
@@ -50,7 +51,7 @@ public:
 	}
 
 	explicit inline ImageIterator(int x, int y, int channel, uint32_t width, int amountOfChannel,
-								  const Image &indexed) : relativeRow(0), startRow(y), relativeCol(0), startCol(x), channel(0), startChannel(channel),
+								  Image const *indexed) : relativeRow(0), startRow(y), relativeCol(0), startCol(x), channel(0), startChannel(channel),
 														  width(width),
 														  indexed(indexed),
 														  amountOfChannel(amountOfChannel) {
@@ -58,26 +59,28 @@ public:
 	}
 
 	explicit inline ImageIterator(int x, int y, int channel, uint32_t width,
-								  const Image &indexed) : ImageIterator(x, y, channel, indexed.GetWidth(),
-																		indexed.GetChannels() - channel, indexed) {
+								  Image const *indexed) : ImageIterator(x, y, channel, indexed->GetWidth(),
+																		indexed->GetChannels() - channel, indexed) {
 	}
 
-	explicit inline ImageIterator(int x, int y, int channel, const Image &indexed) : ImageIterator(x, y, channel,
-																								   indexed.GetWidth(),
+	explicit inline ImageIterator(int x, int y, int channel, Image const *indexed) : ImageIterator(x, y, channel,
+																								   indexed->GetWidth(),
 																								   indexed) {
 	}
 
-	explicit inline ImageIterator(int x, int y, const Image &indexed) : ImageIterator(x, y, 0, indexed) {
+	explicit inline ImageIterator(int x, int y, Image const *indexed) : ImageIterator(x, y, 0, indexed) {
 	}
 
-	explicit inline ImageIterator(const Image &indexed) : ImageIterator(0, 0, indexed) {
+	explicit inline ImageIterator(Image const *indexed) : ImageIterator(0, 0, indexed) {
 	}
-	inline ImageIterator(const ImageIterator &image) : ImageIterator(image.getRelativeCol(), image.getRelativeRow(), image.getRelativeChannel(), image.indexed) {
-		this->startCol = image.startCol;
-		this->startRow = image.startRow;
-		this->startChannel = image.startChannel;
-		this->width = image.width;
-		this->amountOfChannel = image.amountOfChannel;
+	explicit inline ImageIterator(const Image &indexed) : ImageIterator(&indexed) {
+	}
+	inline ImageIterator(const ImageIterator &iterator) : ImageIterator(iterator.getRelativeCol(), iterator.getRelativeRow(), iterator.getRelativeChannel(), iterator.indexed) {
+		this->startCol = iterator.startCol;
+		this->startRow = iterator.startRow;
+		this->startChannel = iterator.startChannel;
+		this->width = iterator.width;
+		this->amountOfChannel = iterator.amountOfChannel;
 	}
 	inline pixel &atOther(Image &otherImage) const {
 		return otherImage(getCol(), getRow(), getChannel());
@@ -85,25 +88,42 @@ public:
 	inline void copyCurrentValue(Image &otherImage) const {
 		this->atOther(otherImage) = **this;
 	}
-	//Will default to 1, especially when we don't need it, for end typically ?
-	pixel operator*() const;
-
-	pixel &operator*();
+	const pixel &operator*() const;
 
 	ImageIterator operator++();
 
-	bool operator!=(ImageIterator other) const;
+	bool operator!=(const ImageIterator& other) const;
+	inline bool operator==(const ImageIterator& other) const {
+		return !(*this != other);
+	}
+	ImageIterator &operator=(const ImageIterator &other) {
+		if (this != &other) {
+			this->startCol = other.startCol;
+			this->startRow = other.startRow;
+			this->startChannel = other.startChannel;
+			this->width = other.width;
+			this->amountOfChannel = other.amountOfChannel;
+			this->indexed = other.indexed;
+			this->relativeCol = other.relativeCol;
+			this->relativeRow = other.relativeRow;
+			this->channel = other.channel;
+			return *this;
+		}
+	}
 
-	std::tuple<ImageIterator, ImageIterator> filterIterators(int filterExtent);
+	std::tuple<ImageIterator, ImageIterator> filterIterators(int filterExtent) const;
 
 	//Because why not, equivalent to filterIterators(0) actually
-	std::tuple<ImageIterator, ImageIterator> pixelValues() {
+	std::tuple<ImageIterator, ImageIterator> pixelValues() const {
 		return {ImageIterator(relativeCol, relativeRow, 0, 1, indexed), ImageIterator(relativeCol, relativeRow + 1, 0, indexed)};
 	}
 };
 
+inline ImageIterator end(Image const *image) {
+	return ImageIterator(0, image->GetHeight(), 0, image);
+}
 inline ImageIterator end(const Image &image) {
-	return ImageIterator(0, image.GetHeight(), 0, image);
+	return end(&image);
 }
 
 
--- Project/src/ImageIterator.cpp
+++ Project/src/ImageIterator.cpp
@@ -2,12 +2,8 @@
 
 //Will default to 1, especially when we don't need it, for end typically ?
 
-pixel ImageIterator::operator*() const {
-    return *(*this);
-}
-
-pixel &ImageIterator::operator*() {
-    return indexed(getCol(), getRow(), getChannel());
+const pixel &ImageIterator::operator*() const {
+    return (*indexed).at(getCol(), getRow(), getChannel());
 }
 
 ImageIterator ImageIterator::operator++() {
@@ -23,16 +19,16 @@ ImageIterator ImageIterator::operator++() {
     return *this;
 }
 
-bool ImageIterator::operator!=(ImageIterator other) const {
+bool ImageIterator::operator!=(const ImageIterator& other) const {
     return other.getRow() != this->getRow() || other.getCol()!= this->getCol() ||
            other.getChannel() != this->getChannel();
 }
 
-std::tuple<ImageIterator, ImageIterator> ImageIterator::filterIterators(int filterExtent) {
+std::tuple<ImageIterator, ImageIterator> ImageIterator::filterIterators(int filterExtent) const{
     int startX = std::max(0, relativeCol - filterExtent);
     int startY = std::max(0, relativeRow - filterExtent);
-    int endX = std::min(static_cast<uint32_t>(relativeCol + filterExtent + 1), indexed.GetWidth());
-    int endY = std::min(static_cast<uint32_t>(relativeRow + filterExtent + 1), indexed.GetHeight());
+    int endX = std::min(static_cast<uint32_t>(relativeCol + filterExtent + 1), indexed->GetWidth());
+    int endY = std::min(static_cast<uint32_t>(relativeRow + filterExtent + 1), indexed->GetHeight());
     auto begin = ImageIterator(startX, startY, channel,
                                endX - startX,1, indexed);
     //We don't need maxWidth nor channel, only used for comparison of a non moving iterator
