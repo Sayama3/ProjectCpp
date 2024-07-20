de5c5f7e62fd9b24d71a985c2b7a1fb6e2fdbc67
superzero4 <78915984+superzero4@users.noreply.github.com>
2024-07-20 23:20:11 +0200

    Create the T2 command and intFloat command

--- Project/include/Scripting/TemplatedCommands.hpp
+++ Project/include/Scripting/TemplatedCommands.hpp
@@ -11,16 +11,16 @@
 #define PC_UINT_REG R"((\d+))"
 
 namespace Pic {
-
 	template<class Self, typename T>
 	class TCommand : public Command {
 	public:
 		TCommand(std::string variableSrc, std::string variableTrg, T value) : variableSource(std::move(variableSrc)), variableTarget(std::move(variableTrg)), value(value) {}
 		[[nodiscard]] virtual std::string GetSource() const override { return variableSource; }
 		[[nodiscard]] virtual std::string GetTarget() const override { return variableTarget; }
+
 	public:
 		[[nodiscard]] static bool Compare(const std::string &content) { return std::regex_search(content, Self::GetComparer()); }
-		[[nodiscard]] static Command* Create(const std::string &content) {
+		[[nodiscard]] static Command *Create(const std::string &content) {
 			std::smatch smatch;
 			if (!std::regex_search(content, smatch, Self::GetComparer())) return nullptr;
 			return new Self(smatch[2], smatch[3], Self::StringToValue(smatch[1]));
@@ -35,6 +35,27 @@ namespace Pic {
 	protected:
 		std::string variableSource, variableTarget;
 		T value;
+	};
+
+	template<class Self, typename T1, typename T2>
+	class T2Command : public TCommand<Self, T1> {
+	public:
+		T2Command(std::string variableSrc, std::string variableTrg, T1 v1, T2 v2) : TCommand<Self, T1>(variableSrc, variableTrg, v1), value2(v2) {}
+
+	public:
+		[[nodiscard]] static Command *Create(const std::string &content) {
+			std::smatch smatch;
+			if (!std::regex_search(content, smatch, Self::GetComparer())) return nullptr;
+			return new Self(smatch[3], smatch[4], Self::StringToValue(smatch[1]), Self::StringToValue2(smatch[2]));
+		}
+		[[nodiscard]] static std::string Save(const Command *command) {
+			const auto *c = dynamic_cast<const Self *>(command);
+			if (!c) return "";
+			return std::format("{} {} {} {} in {}", Self::GetCommandName(), c->value, c->value2, c->variableSource, c->variableTarget);
+		}
+
+	protected:
+		T2 value2;
 	};
 
 	template<class Self>
@@ -66,7 +87,22 @@ namespace Pic {
 	protected:
 		std::string variableSource, variableTarget;
 	};
-
+	template<class Self>
+	class IntFloatCommand : public T2Command<Self, int, float> {
+	public:
+		IntFloatCommand(std::string variableSrc, std::string variableTrg, int value, float value2) : T2Command<Self, int, float>(variableSrc, variableTrg, value, value2) {}
+		[[nodiscard]] static std::regex GetComparer() {
+			static std::string regex = std::format(R"(\s*{}\s+([-+]?\d+)\s+((?:(?:\.\d+)|(?:\d+(?:\.\d*)?)))\s+(\w+)\s+in\s+(\w+)\s*)", Self::GetCommandName());
+			static auto reg = std::regex(regex, std::regex_constants::icase);
+			return reg;
+		}
+		static int StringToValue(const std::string &value) {
+			return std::stoi(value);
+		}
+		static float StringToValue2(const std::string &value) {
+			return std::stof(value);
+		}
+	};
 	template<class Self>
 	class IntCommand : public TCommand<Self, int> {
 	public:
@@ -90,22 +126,39 @@ namespace Pic {
 		}
 		static float StringToValue(const std::string &value) { return std::stof(value); }
 	};
-}
+}// namespace Pic
 
-#define PC_IMPL_FLT_COMMAND(ClassName, Func, commandName) public:\
+#define PC_IMPL_INT_FLT_COMMAND(ClassName, Func, commandName)                                                                                                     \
+public:                                                                                                                                                           \
+	ClassName(std::string variableSrc, std::string variableTrg, int value, float value2) : IntFloatCommand<ClassName>(variableSrc, variableTrg, value, value2) {} \
+	[[nodiscard]] virtual Image Execute(const Image &sc) override { return Func(sc, value, value2); }                                                             \
+	[[nodiscard]] static std::string GetCommandName() { return commandName; }                                                                                     \
+	[[nodiscard]] std::string GetName() { return GetCommandName(); }
+
+#define PC_IMPL_INT_FLT_AS_INT_COMMAND(ClassName, Func, commandName)                                                                                              \
+public:                                                                                                                                                           \
+	ClassName(std::string variableSrc, std::string variableTrg, int value, float value2) : IntFloatCommand<ClassName>(variableSrc, variableTrg, value, value2) {} \
+	[[nodiscard]] virtual Image Execute(const Image &sc) override { return Func(sc, value, static_cast<int>(value2)); }                                           \
+	[[nodiscard]] static std::string GetCommandName() { return commandName; }                                                                                     \
+	[[nodiscard]] std::string GetName() { return GetCommandName(); }
+
+#define PC_IMPL_FLT_COMMAND(ClassName, Func, commandName)                                                                                  \
+public:                                                                                                                                    \
 	ClassName(std::string variableSrc, std::string variableTrg, float value) : FloatCommand<ClassName>(variableSrc, variableTrg, value) {} \
-[[nodiscard]] virtual Image Execute(const Image& sc) override {return Func(sc, value);} \
-[[nodiscard]] static std::string GetCommandName() {return commandName;} \
-[[nodiscard]] std::string GetName() {return GetCommandName();}
+	[[nodiscard]] virtual Image Execute(const Image &sc) override { return Func(sc, value); }                                              \
+	[[nodiscard]] static std::string GetCommandName() { return commandName; }                                                              \
+	[[nodiscard]] std::string GetName() { return GetCommandName(); }
 
-#define PC_IMPL_INT_COMMAND(ClassName, Func, commandName) public:\
+#define PC_IMPL_INT_COMMAND(ClassName, Func, commandName)                                                                              \
+public:                                                                                                                                \
 	ClassName(std::string variableSrc, std::string variableTrg, int value) : IntCommand<ClassName>(variableSrc, variableTrg, value) {} \
-	[[nodiscard]] virtual Image Execute(const Image& sc) override {return Func(sc, value);} \
-	[[nodiscard]] static std::string GetCommandName() {return commandName;} \
-	[[nodiscard]] std::string GetName() {return GetCommandName();}
+	[[nodiscard]] virtual Image Execute(const Image &sc) override { return Func(sc, value); }                                          \
+	[[nodiscard]] static std::string GetCommandName() { return commandName; }                                                          \
+	[[nodiscard]] std::string GetName() { return GetCommandName(); }
 
-#define PC_IMPL_NO_PARAM_COMMAND(ClassName, Func, commandName) public:\
+#define PC_IMPL_NO_PARAM_COMMAND(ClassName, Func, commandName)                                                            \
+public:                                                                                                                   \
 	ClassName(std::string variableSrc, std::string variableTrg) : TCommandNoParam<ClassName>(variableSrc, variableTrg) {} \
-	[[nodiscard]] virtual Image Execute(const Image& sc) override {return Func(sc);} \
-	[[nodiscard]] static std::string GetCommandName() {return commandName;} \
-	[[nodiscard]] std::string GetName() {return GetCommandName();}
+	[[nodiscard]] virtual Image Execute(const Image &sc) override { return Func(sc); }                                    \
+	[[nodiscard]] static std::string GetCommandName() { return commandName; }                                             \
+	[[nodiscard]] std::string GetName() { return GetCommandName(); }
